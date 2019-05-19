ef41d7449cb1144a5ae8b9f2823718e39d8146a4
boneswvb@gmail.com <boneswvb@gmail.com>
2019-05-19 15:39:04 +0200

    final for today

---
+++ src/FilmsInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FilmsInfo = ({ filmData }) => {
+	return(
+		<div className='tc bg-light-blue dib br3 pa3 ma3 grow bw2 shadow-5'>
+			<p>Halllooo!!!!</p>
+			{console.log(filmData)}
+		</div>
+	);
+};
+
+export default FilmsInfo;
